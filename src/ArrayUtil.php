70d76fe4facff53574f2684465c84fc49fb45381
y-uti <y-uti@users.noreply.github.com>
2014-12-14 12:05:27 +0900

    Fix Bug in ArrayUtil.php

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -5,11 +5,16 @@ class ArrayUtil
 {
     public static function cartesianProduct(array $arrays)
     {
+        return !$arrays ? array() : self::cartesianProduct_rec($arrays);
+    }
+
+    private static function cartesianProduct_rec(array $arrays)
+    {
         if (!$arrays) {
             yield array();
         } else {
             $tail = array_pop($arrays);
-            foreach (self::cartesianProduct($arrays) as $values) {
+            foreach (self::cartesianProduct_rec($arrays) as $values) {
                 foreach ($tail as $v) {
                     yield array_merge($values, array($v));
                 }
--- tests/ArrayUtilTest.php
+++ tests/ArrayUtilTest.php
@@ -3,14 +3,45 @@ namespace YUti\OpChecker;
 
 class ArrayUtilTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCartesianProduct()
+    private function toArray($arrayOrGenerator)
+    {
+        $array = array();
+        foreach ($arrayOrGenerator as $v) {
+            $array[] = $v;
+        }
+
+        return $array;
+    }
+
+    public function testCartesianProduct_empty()
+    {
+        $arrays = array();
+        $expected = array();
+        $actual = ArrayUtil::cartesianProduct($arrays);
+        $this->assertEquals($expected, $this->toArray($actual));
+    }
+
+    public function testCartesianProduct_oneArray()
+    {
+        $arrays = array(
+            array(1, 2, 3),
+        );
+        $expected = array(
+            array(1),
+            array(2),
+            array(3),
+        );
+        $actual = ArrayUtil::cartesianProduct($arrays);
+        $this->assertEquals($expected, $this->toArray($actual));
+    }
+
+    public function testCartesianProduct_manyArrays()
     {
         $arrays = array(
             array(1, 2, 3),
             array(4),
             array(5, 6),
         );
-
         $expected = array(
             array(1, 4, 5),
             array(1, 4, 6),
@@ -19,11 +50,19 @@ class ArrayUtilTest extends \PHPUnit_Framework_TestCase
             array(3, 4, 5),
             array(3, 4, 6),
         );
-
         $actual = ArrayUtil::cartesianProduct($arrays);
-        $i = 0;
-        foreach ($actual as $a) {
-            $this->assertEquals($expected[$i++], $a);
-        }
+        $this->assertEquals($expected, $this->toArray($actual));
+    }
+
+    public function testCartesianProduct_containsEmptyArray()
+    {
+        $arrays = array(
+            array(1, 2, 3),
+            array(),
+            array(5, 6),
+        );
+        $expected = array();
+        $actual = ArrayUtil::cartesianProduct($arrays);
+        $this->assertEquals($expected, $this->toArray($actual));
     }
 }
